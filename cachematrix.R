84bf6c01c9ae5d372532a4a59babeaf1638a01eb
raj_choyal123 <raj_choyal123@yahoo.com>
2020-12-15 09:40:37 +0530

    coit with chnages

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function takes matrix type argument
+## and return special type of list with
+## that provides following functions
+##1.set the value of the matrix
+## 2.get the value of the matrix
+## 3.set the value of the inverse of matrix
+## 4.get the value of the inverse of matrix
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  if(det(x)==0)
+  {
+    return(NULL)
+    
+  }
+  
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function(){
+    x
+  } 
+  setinverse <- function(inverse){
+    
+    inv <<- inverse
+  } 
+  getinverse <- function(){
+    inv
+  } 
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
+  
 
 }
 
@@ -12,4 +45,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  inv <- x$getinverse()
+  
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
+  
+  
+  
 }
